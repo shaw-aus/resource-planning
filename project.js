391ad5e569c2b4aac21a1c925bccd468d71bcfbe
shaw-aus <stewart_shaw@outlook.com>
2024-07-31 15:26:44 +1000

    updated project html and js as well as report.js

--- project.js
+++ project.js
@@ -3,16 +3,10 @@ document.getElementById('addProjectForm').addEventListener('submit', function (e
 
     const projectName = document.getElementById('projectName').value;
     const clientName = document.getElementById('clientName').value;
-    const projectStartDate = document.getElementById('projectStartDate').value;
-    const projectEndDate = document.getElementById('projectEndDate').value;
-    const budget = document.getElementById('budget').value;
 
     const projectData = {
         projectName,
-        clientName,
-        projectStartDate,
-        projectEndDate,
-        budget
+        clientName
     };
 
     let projects = JSON.parse(localStorage.getItem('projects')) || [];
